0f43732bfe28ba7e77ffea09f5cdea5ab5af065e
Aman Singh <amansingh81757@gmail.com>
2024-06-23 17:23:30 +0530

    update loan formula

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -5,9 +5,15 @@ import toolStore from '../stores/toolStore'
 
 export default function Tools() {
     const store = toolStore();
+
+    const emi = Math.trunc(store.emi);
+    const loanAmount = Math.trunc(store.loanAmount);
+    const loanTenure = Math.trunc(store.loanTenure);
+    const totalAmount = emi * 12 * loanTenure;
+    const totalInterest = totalAmount - loanAmount;
     const data1 = [
         { id: 0, value: Number(store.loanAmount), label: 'Principal Amount' },
-        { id: 1, value: Number(store.emi) * Number(store.loanTenure) / 12, label: 'Interest Amount' },
+        { id: 1, value: Number(totalInterest), label: 'Interest Amount' },
     ]
     return (
         <div className='p-10'>
@@ -37,7 +43,7 @@ export default function Tools() {
                             <Sliders
                                 min={1}
                                 max={100}
-                                step={0.5}
+                                step={0.2}
                                 defaultValueType={'rateOfInterest'}
                                 label='Rate of interest (p.a)'
                                 onChangeFunction={(e, value) => {store.changeRateOfInterest(value); console.log(typeof value)}}
@@ -53,6 +59,12 @@ export default function Tools() {
                                 onChangeFunction={(e, value) => store.changeLoanTenure(value)}
                             />
                         </div>
+                    </div>
+                    <div className='flex flex-col'>
+                        <h1>Montly EMI: {emi}</h1>
+                        <h1>Principal Amount: {loanAmount}</h1>
+                        <h1>Total Interest: {totalInterest}</h1>
+                        <h1>Total Amount: {totalAmount}</h1>
                     </div>
                     <div className='flex justify-center items-center'>
                         <BasicPie data = {data1}/>
--- src/stores/toolStore.js
+++ src/stores/toolStore.js
@@ -3,17 +3,17 @@ import axios from 'axios';
 
 const toolStore = create((set) => ({
 
-    loanAmount: 1000,
-    rateOfInterest: 1,
-    loanTenure: 1,
-    emi: 0,
+    loanAmount: 1000000,
+    rateOfInterest: 7.2,
+    loanTenure: 10,
+    emi: 11714,
 
     getEmi: () => {
         const P = toolStore.getState().loanAmount;
-        const R = toolStore.getState().rateOfInterest;
-        const N = toolStore.getState().loanTenure;
-        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N-1));
-        // const emi = P + R+ N;
+        const R = toolStore.getState().rateOfInterest/1200;
+        const N = toolStore.getState().loanTenure*12;
+        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N)-1);
+        // const emi = Math.pow(1 + R, N);
         set({emi});
     },
 
@@ -21,30 +21,30 @@ const toolStore = create((set) => ({
         amount = Number(amount)
         set({loanAmount: amount});
         const P = toolStore.getState().loanAmount;
-        const R = toolStore.getState().rateOfInterest;
-        const N = toolStore.getState().loanTenure;
-        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N-1));
-        // const emi = P + R+ N;
+        const R = toolStore.getState().rateOfInterest/1200;
+        const N = toolStore.getState().loanTenure*12;
+        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1);
+        // const emi = Math.pow(1 + R, N);
         set({emi});
     },
     changeRateOfInterest: (rate) => {
         rate = Number(rate)
         set({rateOfInterest: rate});
         const P = toolStore.getState().loanAmount;
-        const R = toolStore.getState().rateOfInterest;
-        const N = toolStore.getState().loanTenure;
-        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N-1));
-        // const emi = P + R+ N;
+        const R = toolStore.getState().rateOfInterest/1200;
+        const N = toolStore.getState().loanTenure*12;
+        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1);
+        // const emi = Math.pow(1 + R, N);
         set({emi});
     },
     changeLoanTenure: (tenure) => {
         tenure = Number(tenure)
         set({loanTenure: tenure});
         const P = toolStore.getState().loanAmount;
-        const R = toolStore.getState().rateOfInterest;
-        const N = toolStore.getState().loanTenure;
-        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N-1));
-        // const emi = P + R+ N;
+        const R = toolStore.getState().rateOfInterest/1200;
+        const N = toolStore.getState().loanTenure*12;
+        const emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1);
+        // const emi = Math.pow(1 + R, N);
         set({emi});
     },
 
